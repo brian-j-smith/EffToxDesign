a69991825b161a4f167dc7a743e1b48915fddf67
Brian J Smith <brian-j-smith@uiowa.edu>
2018-05-24 11:00:41 -0500

    Split Design source files

---
+++ R/EffToxSim.R
@@ -0,0 +1,34 @@
+EffToxSim <- R6Class("EffToxSim",
+  public = list(
+    design = NULL,
+    truth = NULL,
+    outcomes = NULL,
+   
+   
+    initialize = function(design, eff, tox, outcomes) {
+      self$design <- design
+      self$truth <- data.frame(eff = eff, tox = tox)
+      self$outcomes <- outcomes
+      invisible(self)
+    },
+   
+   
+    summary = function() {
+     
+      .extract <- function(name) {
+        unlist(lapply(self$outcomes, function(outcome) outcome[[name]]))
+      }
+     
+      n <- length(self$outcomes)
+      doses_selected <- factor(.extract("dose"), self$design$doses)
+      doses_given <- factor(.extract("doses_given"), self$design$doses)
+      .simsummary(self$truth$eff,
+                  self$truth$tox,
+                  table(doses_selected) / n,
+                  prop.table(table(doses_given)),
+                  tapply(.extract("yE"), doses_given, sum) / n,
+                  tapply(.extract("yT"), doses_given, sum) / n,
+                  table(doses_given) / n)
+    }
+  )
+)
--- R/TwoPhaseDesign.R
+++ R/TwoPhaseDesign.R
@@ -39,31 +39,3 @@ TwoPhaseDesign <- R6Class("TwoPhaseDesign",
     }
   )
 )
-
-
-TwoPhaseSim <- R6Class("TwoPhaseSim",
-  public = list(
-    design = NULL,
-    truth = NULL,
-    outcomes = NULL,
-   
-   
-    initialize = function(design, eff, tox, outcomes) {
-      self$design <- design
-      self$truth <- data.frame(eff = eff, tox = tox)
-      self$outcomes <- outcomes
-      invisible(self)
-    },
-   
-   
-    summary = function() {
-      .simsummary(self$truth$eff,
-                  self$truth$tox,
-                  self$outcomes$stage1_selected * self$outcomes$stage2_selected,
-                  prop.table(self$outcomes$n_average),
-                  self$outcomes$n_average * self$truth$eff,
-                  self$outcomes$n_average * self$truth$tox,
-                  self$outcomes$n_average)
-    }
-  )
-)
